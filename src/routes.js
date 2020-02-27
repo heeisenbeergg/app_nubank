49e1015dbd356ca21a1b2157c6ed94988009f4af
Lucas <lucaswillian45@gmail.com>
2020-02-27 20:07:09 -0300

    feat: create routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Main from './pages/Main'
+
+const Stack = createStackNavigator()
+
+export default function Routes() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="main" component={Main} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+}
